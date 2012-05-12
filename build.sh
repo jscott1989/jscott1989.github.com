1c4a032047cc78b506e64b5f04b036e78484d964
Jonathan Scott <jonathanscott1989@gmail.com>
2012-05-12 21:28:45 +0100

    Update content

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
+git add .
 git commit -am "Update content"
 git push
 jekyll --no-auto
 git checkout master
 cp -R _site/* .
+git add .
 git commit -am "Update content"
 git push
 git checkout source
